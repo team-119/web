c8465ade4b2626ee28f3ace1bf987c47adbf4ccd
Minji Park <pmj02312@gmail.com>
2020-10-25 00:15:06 +0900

    10.25 지금 상태...

--- emerapp/models.py
+++ emerapp/models.py
@@ -6,15 +6,15 @@ class Patient(models.Model):
     hos = models.CharField(max_length=1, null=True)
     by = models.IntegerField(default=0) #0이면 구급대원, 1이면 병
     
-    ETE_S = models.IntegerField(default=0)
-    ETE_C = models.IntegerField(default=0)
-    ETE_B = models.IntegerField(default=0)
-    ETE_U = models.IntegerField(default=0)
+    ETE_S = models.CharField(default=0, max_length=5)
+    ETE_C = models.CharField(default=0, max_length=5)
+    ETE_B = models.CharField(default=0, max_length=5)
+    ETE_U = models.CharField(default=0, max_length=5)
     
-    ETA_S = models.IntegerField(default=0)
-    ETA_C = models.IntegerField(default=0)
-    ETA_B = models.IntegerField(default=0)
-    ETA_U = models.IntegerField(default=0)
+    ETA_S = models.CharField(default=0, max_length=5)
+    ETA_C = models.CharField(default=0, max_length=5)
+    ETA_B = models.CharField(default=0, max_length=5)
+    ETA_U = models.CharField(default=0, max_length=5)
     
     def __str__(self):
         return str(self.id)
